ff9004ffc2054ba70c103bd75875b0c52928614b
ty <tyrone.lu01@gmail.com>
2026-01-01 11:03:41 +0800

    add script to control and save brightess of both screens

---
+++ duo-brightness.sh
@@ -0,0 +1,17 @@
+
+if [ -z $1 ]; then
+  echo "Testing."
+  exit 0
+fi
+
+monitor_1=$1
+monitor_2=$1
+
+if ! [ -z $2 ]; then
+  monitor_2=$2
+fi
+
+brightnessctl -d intel_backlight set $monitor_1
+brightnessctl -d intel_backlight -s
+brightnessctl -d card1-eDP-2-backlight set $monitor_2
+brightnessctl -d card1-eDP-2-backlight -s 
